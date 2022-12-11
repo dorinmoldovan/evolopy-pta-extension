151966f4d40c8bbf198477bc9bbe49aafd934ac8
dorinmoldovan <dorin.moldovan@yahoo.com>
2022-12-11 21:05:48 +0200

    PTA version 1

--- simulator.py
+++ simulator.py
@@ -12,10 +12,10 @@ from optimizer import run
 optimizer = ["PSO", "GWO", "CS", "CSA", "HOA", "PTA"]
 
 # Select benchmark function"
-# "f1", "f1", "f2", "f3", "f4", "f5", "f6", "f7"
+# "f1", "f2", "f3", "f4", "f5", "f6", "f7"
 # "g1", "g2", "g3", "g4", "g5", "g6", "g7"
 # "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"
-objectivefunc = ["f1"]
+objectivefunc = ["f1", "f2", "f3", "f4", "f5", "f6", "f7"]
 
 # Select number of repetitions for each experiment.
 # To obtain meaningful statistical results, usually 30 independent runs are executed for each algorithm.
