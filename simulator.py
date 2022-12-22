6d3b548ac604ef2933c8127aa7b25cda37b0a327
dorinmoldovan <dorin.moldovan@yahoo.com>
2022-12-22 08:15:48 +0200

    PTA revised

--- simulator.py
+++ simulator.py
@@ -9,7 +9,7 @@ from optimizer import run
 
 # Select optimizers
 # "PSO", "GWO", "CS", "CSA", "HOA", "PTA"
-optimizer = ["GWO", "CS", "CSA", "PTA"]
+optimizer = ["GWO", "PTA"]
 
 # Select benchmark function"
 # "f1", "f2", "f3", "f4", "f5", "f6", "f7"
