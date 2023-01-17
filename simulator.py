4c30b4e8a6ad5450a89f975ecb9e7b24ca41fefd
dorinmoldovan <dorin.moldovan@yahoo.com>
2023-01-17 21:23:34 +0200

    updated the charts generators

--- simulator.py
+++ simulator.py
@@ -8,8 +8,8 @@ Created on Tue May 17 15:50:25 2016
 from optimizer import run
 
 # Select optimizers
-# "PSO", "CSO", "GWO", "CS", "CSA", "HOA", "PTA"
-optimizer = ["PSO", "CSO", "GWO", "CS", "CSA", "HOA", "PTA"]
+# "CSO", "PSO", "GWO", "CS", "CSA", "HOA", "PTA"
+optimizer = ["CSO", "PSO", "GWO", "CS", "CSA", "HOA", "PTA"]
 
 # Select benchmark function"
 # "f1", "f2", "f3", "f4", "f5", "f6", "f7"
