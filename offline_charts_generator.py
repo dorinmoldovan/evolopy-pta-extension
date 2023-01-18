da169c73480355c406cc17ea2f5a41b0b17993cb
dorinmoldovan <12948353+dorinmoldovan@users.noreply.github.com>
2023-01-18 23:09:26 +0200

    revised the .py file

--- offline_charts_generator.py
+++ offline_charts_generator.py
@@ -9,12 +9,13 @@ optimizer = ["CSO", "PSO", "GWO", "CS", "CSA", "HOA", "PTA"]
 # "f1", "f2", "f3", "f4", "f5", "f6", "f7"
 # "g1", "g2", "g3", "g4", "g5", "g6", "g7"
 # "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"
-objectivefunc = ["f1"]
+objectivefunc = ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "g1", "g2", "g3", "g4", "g5", "g6", "g7",
+                 "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10"]
 
 # number of iterations
 Iterations = 1000
 
-results_directory = "results/final_results_f1"
+results_directory = "results/final_results"
 
 conv_plot.run(results_directory, optimizer, objectivefunc, Iterations)
 box_plot.run(results_directory, optimizer, objectivefunc, Iterations)
